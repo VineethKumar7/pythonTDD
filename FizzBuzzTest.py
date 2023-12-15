9c61b90f447aa3e0c2aee70ae0c1ad6af0fd6152
VineethKumar7 <k.vineeth26@gmail.com>
2023-12-15 11:44:36 +0100

    first commit

---
+++ FizzBuzzTest.py
@@ -0,0 +1,11 @@
+import  pytest
+def fizzBuzz(value):
+    return str(value)
+
+def test_returns1With1PassedIn():
+    retVal = fizzBuzz(1)
+    assert retVal == "1"
+
+def test_returns2With2PassedIn():
+    retVal = fizzBuzz(2)
+    assert retVal == "2"
